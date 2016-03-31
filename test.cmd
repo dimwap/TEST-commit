4f9a55404bf75ba52e680378614641ba66a2a009
dimwap <dimwap@mail.ru>
2016-04-01 01:00:00 +0104

    test

--- test.cmd
+++ test.cmd
@@ -3,6 +3,6 @@ git commit -m "test"
 GIT_COMMITTER_DATE="Fri Apr 1 01:00 2016 +0104" 
 git commit --amend --date="Fri Apr 1 01:00 2016 +0104"
 
-git remote add origin https://github.com/dimwap/TEST-commit.git
-git branch -M main
-git push -u origin main
+:git remote add origin https://github.com/dimwap/TEST-commit.git
+:git branch -M main
+:git push -u origin main
