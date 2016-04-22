e590937886c5f65d17007d246a619952fb776684
dimwap <dimwap@mail.ru>
2016-04-22 02:07:00 +0104

    test

--- test.cmd
+++ test.cmd
@@ -1,11 +1,11 @@
 git add -A
 git commit -m "test"
-set GIT_COMMITTER_DATE="Wed May 4 02:07 2016 +0104" 
+set GIT_COMMITTER_DATE="Fri Apr 22 02:07 2016 +0104" 
 git commit --amend --date=%GIT_COMMITTER_DATE%
 
 :"Wed Apr 20 02:04 2016 +0104"
 
-git push -u
+:git push -u
 
 :git remote add origin https://github.com/dimwap/TEST-commit.git
 :git branch -M main
