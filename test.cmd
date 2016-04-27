15e4c2dd96ae434110843588d41e427923ed3c81
dimwap <dimwap@mail.ru>
2016-04-27 02:06:00 +0104

    test

--- test.cmd
+++ test.cmd
@@ -1,6 +1,6 @@
 git add -A
 git commit -m "test"
-set GIT_COMMITTER_DATE="Wed Apr 20 02:05 2016 +0104" 
+set GIT_COMMITTER_DATE="Wed Apr 27 02:06 2016 +0104" 
 git commit --amend --date=%GIT_COMMITTER_DATE%
 
 :"Wed Apr 20 02:04 2016 +0104"
